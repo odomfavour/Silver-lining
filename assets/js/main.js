e0361c7f9f834434ae64077e22c8ca9f9b2c3ec4
odomfavour <ogochukwuodom@gmail.com>
2020-07-13 18:40:23 +0100

    feat: made changes to some styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const typedTextSpan = document.querySelector('.typed-text');
-const textArray = ["Rebecca Wook", "Web Developer", "Creative", "Awesome"];
+const textArray = ["Ogochukwu Odom", "A Web Developer", "Creative", "Awesome"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newtextDelay = 2000;
@@ -34,50 +34,5 @@ document.addEventListener('DOMContentLoaded', function () {
   setTimeout(type, newtextDelay + 250)
 })
 
-jQuery(document).ready(function ($) {
-  
-  
-  var navclone = function() {
-    $('.js-clone-nav').each(function () {
-      var $this = $(this);
-      $this.clone().attr('class', 'clone-view').appendTo('.mobile-view-body');
-    });
 
-    // document.querySelector('.js-toggle').addEventListener('click', openNav)
-
-    // function openNav() {
-    //   document.querySelector('.mobile-view').classList.add(open);
-    // }
-    $('body').on('click', '.js-toggle', function (e) {
-      var $this = $(this);
-      e.preventDefault();
-      if ($('.mobile-view').hasClass('.open')) {
-        $('.mobile-view').removeClass('.open')
-      } else {
-        $('.mobile-view').addClass('.open')
-      }
-    })
-  }
-  navclone()
-})
-
-
-const menuBtn = document.querySelector('.mobile-menu');
-const closeBtn = document.querySelector('.mobile-view-close');
-const navLink = document.querySelector('.nav-item');
-const display = document.querySelector('.mobile-view');
-
-let showMenu = false;
-menuBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
-navLink.addEventListener('click', toggleMenu);
-function toggleMenu() {
-  if (!showMenu) {
-    display.classList.add('open')
-    showMenu = true;
-  } else {
-    display.classList.remove('open')
-    showMenu = false;
-  }
-}
 
